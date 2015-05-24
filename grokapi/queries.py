eb1ac8dc8376642f59c4662a6e5c19f2e5f9161e
Jean-Fred <JeanFred@github>
2015-05-24 18:53:36 +0200

    Add method make_url_latest

--- grokapi/queries.py
+++ grokapi/queries.py
@@ -15,3 +15,13 @@ class Grok(object):
         """Make the URL to the JSON output of stats.grok.se service."""
         return BASE_URL + "{0:s}/{1:d}{2:02d}/{3:s}".format(self.site, year, month, self.title)
 
+    def _make_url_latest(self, latest):
+        """Make URL to the JSON output of stats.grok.se service for lastest traffic.
+
+        Args:
+            latest (int): 30, 60 or 90
+        """
+        if latest not in [30, 60, 90]:
+            raise ValueError("Expected 30, 60 or 90 instead of %s" % (latest))
+
+        return BASE_URL + "{0:s}/latest{1:d}/{2:s}".format(self.site, latest, self.title)
--- tests/test_queries.py
+++ tests/test_queries.py
@@ -18,6 +18,19 @@ class TestGrok(unittest.TestCase):
         expected = 'http://stats.grok.se/json/fr/201301/France'
         self.assertEqual(result, expected)
 
+    def test_make_url_latest(self):
+        """test make_url_latest()."""
+        grok = grokapi.queries.Grok('France', 'fr')
+        result = grok._make_url_latest(90)
+        expected = 'http://stats.grok.se/json/fr/latest90/France'
+        self.assertEqual(result, expected)
+
+    def test_make_url_latest_with_wrong_value(self):
+        """test make_url_latest() with a wrong value should raise a ValueError Exception."""
+        grok = grokapi.queries.Grok('France', 'fr')
+        with self.assertRaises(ValueError):
+            grok._make_url_latest(42)
+
 
 if __name__ == '__main__':
     unittest.main()
